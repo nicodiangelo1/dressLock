9f151e0bc614d8abe07bb2e8c2ef1c18c6c5fc57
PabloPRF22 <55289636+PabloPRF22@users.noreply.github.com>
2024-08-25 21:34:43 +0200

    Cirreciones mínimas en la internacionalización

--- pull.sh
+++ pull.sh
@@ -6,8 +6,6 @@ eval "$(ssh-agent -s)"
 # Agregar la clave privada SSH al agente
 ssh-add ~/.ssh/git
 
-# Navegar al directorio del repositorio (opcional, si no estás en el directorio del repo)
-# cd /ruta/a/tu/repositorio
 
 # Ruperar cambios
 git pull origin main
